08c53551546e1f523be51fa788ff0c793c22499a
ahrisam <ahrisam3@outlook.com>
2025-09-03 16:21:21 +0000

    second commit
    Updated models.py and added templates and static folder

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='uploads/category')
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length= 250, default='', blank=True)
+    image = models.ImageField(upload_to='uploads/product')
+
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
