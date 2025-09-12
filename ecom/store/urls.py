648894d4d19974f093894faf1027815fd886e4f9
ahrisam <ahrisam3@outlook.com>
2025-09-12 16:20:45 +0000

    added cart app

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('categories/<str:foo>/', views.category, name='category')
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    categories = Category.objects.all()
+
+
+    return render(request, 'index.html',{'products': products,'categories':categories})
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    category = Category.objects.get(name=foo)
+    products = Product.objects.filter(category=category)
+
+    return render(request, 'product.html', {'products':products, 'category':category })
