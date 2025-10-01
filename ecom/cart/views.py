25b350d3eaa3f9b3632c322dd2f85e3a75fa90f5
ahrisam <ahrisam3@outlook.com>
2025-10-01 22:16:16 +0000

    A functional dropdown menu

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
-from django.http import JsonResponse
+from django.http import JsonRespons
+
+
+def cart_add(request):
+    pass
